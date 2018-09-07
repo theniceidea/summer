5faeade2247d9160aed35f14210295ad1a84a14b
xiahongtao <this@live.cn>
2018-09-07 16:59:09 +0800

    async result

--- demovertx2/src/main/java/org/summerframework/demovertx2/srv/TestService4.java
+++ demovertx2/src/main/java/org/summerframework/demovertx2/srv/TestService4.java
@@ -2,11 +2,13 @@ package org.summerframework.demovertx2.srv;
 
 import io.vertx.redis.RedisClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.session.SessionProperties;
 import org.springframework.stereotype.Service;
 import org.summerframework.demovertx2.model.RedisGet;
 import org.summerframework.demovertx2.model.TestModel;
 import org.summerframework.model.SceneStack;
 import org.summerframework.model.SummerService;
+import org.summerframework.model.SkipReturnCheck;
 
 @Service
 @SummerService
@@ -17,6 +19,7 @@ public class TestService4 extends SceneStack{
     private RedisClient redisClient;
     private RedisGet redisGet;
 
+    @SkipReturnCheck
     public void redisGet(RedisGet m){
         //create Hander
 //        redisClient.get(m.getKey(), r -> m.retun(r.succeeded() ? r.result() : null));
@@ -26,11 +29,8 @@ public class TestService4 extends SceneStack{
     public void task2(TestModel model){
         TestService4 stack = model.recovery(TestService4.class);
         if (model.entry(0)) {
-            stack.redisGet = model.a(100)
-                .b(RedisGet.class)
-                .c(redisKey);
-        }
-        else if (model.entry(100)) {
+            stack.redisGet = model.a(100).b(RedisGet.class).c(redisKey);
+        }else if (model.entry(100)) {
             model.retun(stack.redisGet.getSummerResult());
         }
     }
