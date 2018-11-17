eac5f371c03c26b696a78d6fd21bc50806fb5017
jerry <this@live.cn>
2018-11-17 21:53:04 +0800

    summerContext

--- model/src/main/java/org/summerframework/model/Summer.java
+++ model/src/main/java/org/summerframework/model/Summer.java
@@ -1,6 +1,7 @@
 package org.summerframework.model;
 
 public abstract class Summer<R> implements SkipRewrite {
+    private transient Object summerContext;
     private transient Summer summerParent;
     private transient R summerResult;
 
@@ -14,9 +15,19 @@ public abstract class Summer<R> implements SkipRewrite {
 
     public R sum(){ return null; }
 
+    public static  <E extends Summer> E instanceWithContext(Class<E> cls, Object summerContext){
+        try {
+            Summer t = cls.newInstance();
+            t.summerContext = summerContext;
+            return (E) t;
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
     public <E extends Summer> E instanceWithContext(Class<E> cls){
         try {
             Summer t = cls.newInstance();
+            t.summerContext = this.summerContext;
             t.summerParent = this;
             return (E) t;
         } catch (InstantiationException | IllegalAccessException e) {
@@ -30,6 +41,10 @@ public abstract class Summer<R> implements SkipRewrite {
 
     public void setSummerResult(R summerResult) {
         this.summerResult = summerResult;
+    }
+
+    public Object getSummerContext() {
+        return summerContext;
     }
 
     public Summer getSummerParent() {
